d5acb37f9b46f8222c4ce5866531bdfa0b380cf8
Admin <admin@KS-MacBook-Pro.local>
2019-09-24 12:19:59 +0300

    Listview implementation

--- lib/main.dart
+++ lib/main.dart
@@ -10,25 +10,27 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
-        title: Text('Flutter app'),
+        title: Text('Flutter Widget Workout'),
       ),
-      body: Column(
-        // mainAxisAlignment:
-        //     MainAxisAlignment.start, // verticial aligment for column
-        crossAxisAlignment:
-            CrossAxisAlignment.stretch, //horizontal aligment for column
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            child: Card(
-              color: Colors.blue,
-              child: Text('Chart'),
-              elevation: 5,
+      body: SingleChildScrollView(
+              child: Column(
+          // mainAxisAlignment:
+          //     MainAxisAlignment.start, // verticial aligment for column
+          crossAxisAlignment:
+              CrossAxisAlignment.stretch, //horizontal aligment for column
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('Chart'),
+                elevation: 5,
+              ),
             ),
-          ),
-          UserTransactions()
-          
-        ],
+            UserTransactions()
+            
+          ],
+        ),
       ),
     ));
   }
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -4,7 +4,6 @@ import 'package:intl/intl.dart';
 import '../models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
- 
   final List<Transaction> transactions;
 
   TransactionList(this.transactions);
@@ -12,43 +11,48 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(
-              children: transactions.map((tx) {
-            return Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.purple, width: 1)),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      '\$${tx.amount}',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 17,
-                          color: Colors.purple),
-                    ),
+      height: 500,
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
+          Transaction tx = transactions[index];
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.purple, width: 1)),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    '\$${tx.amount}',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17,
+                        color: Colors.purple),
                   ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        tx.title,
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        DateFormat('dd.mm.yyyy H:s').format(tx.date),
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            );
-          }).toList()),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      tx.title,
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      DateFormat('dd.mm.yyyy H:s').format(tx.date),
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+        itemCount: transactions.length,
+        scrollDirection: Axis.vertical,
+      ),
     );
   }
 }
